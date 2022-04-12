b4a7f05be286dfa3550f76cbb34db62c01b0acde
Ali-Akber Saifee <ali@indydevs.org>
2022-04-12 16:53:58 -0700

    Remove benchmark submodule

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ ! -e python-redis-benchmark ];
+then
+    git clone git@github.com:alisaifee/python-redis-benchmark python-redis-benchmark
+fi;
 python -m venv .benchmarks
 source .benchmarks/bin/activate
 pip install -r python-redis-benchmark/requirements.txt
