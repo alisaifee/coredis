4793c2c112d15af9f0df40dfc80dd7fd1f0b42fc
Ali-Akber Saifee <ali@indydevs.org>
2022-05-12 17:48:20 -0700

    Disable keydb tests

--- tests/commands/test_string.py
+++ tests/commands/test_string.py
@@ -14,8 +14,6 @@ from tests.conftest import targets
     "redis_basic_resp3",
     "redis_basic_raw_resp3",
     "redis_cluster",
-    "keydb",
-    "keydb_resp3",
 )
 @pytest.mark.asyncio()
 class TestString:
