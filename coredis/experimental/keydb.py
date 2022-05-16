02f371e12053d8089e3668f639d4572aadd1f9a2
Ali-Akber Saifee <ali@indydevs.org>
2022-05-16 10:31:33 -0700

    Update KeyDB client with missing commands:
    - bitop lshift/rshift
    - pexpireat
    - ttl
    - pttl
    - hrename
    - object lastupdated
    - cron

--- coredis/experimental/keydb.py
+++ coredis/experimental/keydb.py
@@ -3,32 +3,47 @@ from __future__ import annotations
 import datetime
 import functools
 import textwrap
+from typing import Any, Tuple
 
 from packaging import version
 
 from coredis._utils import CaseAndEncodingInsensitiveEnum
 from coredis.client import Redis
-from coredis.commands._utils import check_version, normalized_time_seconds
+from coredis.commands._utils import (
+    check_version,
+    normalized_milliseconds,
+    normalized_time_milliseconds,
+    normalized_time_seconds,
+)
 from coredis.commands._wrappers import ClusterCommandConfig, CommandDetails
 from coredis.commands.constants import CommandGroup
-from coredis.response._callbacks import BoolCallback
+from coredis.response._callbacks import (
+    BoolCallback,
+    BoolsCallback,
+    IntCallback,
+    SimpleStringCallback,
+)
 from coredis.typing import (
     AnyStr,
-    Awaitable,
     Callable,
     CommandArgList,
+    Coroutine,
     Dict,
+    Iterable,
     KeyT,
     Literal,
     Optional,
     P,
+    Parameters,
     R,
+    StringT,
     Union,
+    ValueT,
 )
 
 
 def _keydb_command_link(command: CommandName) -> str:
-    canonical_command = str(command).lower().replace(" ", "-")
+    canonical_command = str(command).lower().replace(" ", "-").replace(".", "")
     return (
         f"`{str(command)} <https://docs.keydb.dev/docs/commands#{canonical_command}>`_"
     )
@@ -36,11 +51,19 @@ def _keydb_command_link(command: CommandName) -> str:
 
 class CommandName(CaseAndEncodingInsensitiveEnum):
     """
-    Enum for listing all redis commands
+    Enum for listing all keydb extension commands
     """
 
+    BITOP = b"BITOP"
+    CRON = b"KEYDB.CRON"
     EXPIREMEMBER = b"EXPIREMEMBER"
     EXPIREMEMBERAT = b"EXPIREMEMBERAT"
+    PEXPIREMEMBERAT = b"PEXPIREMEMBERAT"
+    HRENAME = b"KEYDB.HRENAME"
+    MEXISTS = b"KEYDB.MEXISTS"
+    OBJECT_LASTMODIFIED = b"OBJECT LASTMODIFIED"
+    PTTL = b"PTTL"
+    TTL = b"TTL"
 
 
 def keydb_command(
@@ -52,7 +75,9 @@ def keydb_command(
     arguments: Optional[Dict[str, Dict[str, str]]] = None,
     readonly: bool = False,
     cluster: ClusterCommandConfig = ClusterCommandConfig(),
-) -> Callable[[Callable[P, Awaitable[R]]], Callable[P, Awaitable[R]]]:
+) -> Callable[
+    [Callable[P, Coroutine[Any, Any, R]]], Callable[P, Coroutine[Any, Any, R]]
+]:
     command_details = CommandDetails(
         command_name,
         group,
@@ -63,7 +88,9 @@ def keydb_command(
         cluster or ClusterCommandConfig(),
     )
 
-    def wrapper(func: Callable[P, Awaitable[R]]) -> Callable[P, Awaitable[R]]:
+    def wrapper(
+        func: Callable[P, Coroutine[Any, Any, R]]
+    ) -> Callable[P, Coroutine[Any, Any, R]]:
         @functools.wraps(func)
         async def wrapped(*args: P.args, **kwargs: P.kwargs) -> R:
             check_version(
@@ -97,6 +124,82 @@ class KeyDB(Redis[AnyStr]):
     The client is mostly :class:`coredis.Redis` with a couple of extra
     commands specific to KeyDB.
     """
+
+    @keydb_command(
+        CommandName.BITOP,
+        CommandGroup.BITMAP,
+    )
+    async def bitop(
+        self,
+        keys: Parameters[KeyT],
+        operation: StringT,
+        destkey: KeyT,
+        value: Optional[int] = None,
+    ) -> int:
+        """
+        Perform a bitwise operation using :paramref:`operation` between
+        :paramref:`keys` and store the result in :paramref:`destkey`.
+        """
+        pieces: CommandArgList = [operation, destkey, *keys]
+        if value is not None:
+            pieces.append(value)
+        return await self.execute_command(
+            CommandName.BITOP, *pieces, callback=IntCallback()
+        )
+
+    @keydb_command(
+        CommandName.CRON,
+        CommandGroup.SCRIPTING,
+    )
+    async def cron(
+        self,
+        name: KeyT,
+        repeat: bool,
+        delay: Union[int, datetime.timedelta],
+        script: StringT,
+        keys: Parameters[KeyT],
+        args: Parameters[ValueT],
+        start: Optional[Union[int, datetime.datetime]] = None,
+    ) -> bool:
+        """
+        Schedule a LUA script to run at a specified time and/or intervals.
+        To cancel the cron delete the key at :paramref:`name`
+
+        :param name: Name of the cron which will be visible in the keyspace,
+         can be searched, and deleted with DEL.
+        :param repeat: If the script will run only once, or if it will be repeated
+         at the specified interval provided by :paramref:`delay`
+        :param delay: is an integer specified in milliseconds used as the initial delay.
+         If :paramref:`repeat` is ``True``, this will also be the length of the repeating timer
+         which will execute the script each time the delay elapses
+         (will continue to execute indefinitely).
+        :param start: unix time specified as milliseconds enforcing that the script
+         should only start executing then this Unix time has been reached.
+         If :paramref:`delay` is greater than zero, this delay time will need to elapse prior to the
+         script executing (timer begins to elapse at start time).
+         If a start time is specified, the delay will always remain in reference
+         intervals to that start time.
+        :param script: is the body of the LUA script to execute.
+        :param keys: The keys expected by the script
+        :param args: The args required by the script
+        """
+        pieces: CommandArgList = [name]
+        if repeat:
+            pieces.append(b"REPEAT")
+        else:
+            pieces.append(b"SINGLE")
+        if start is not None:
+            pieces.append(normalized_time_milliseconds(start))
+        pieces.append(normalized_milliseconds(delay))
+        pieces.append(script)
+        _keys = list(keys)
+        pieces.append(len(_keys))
+        pieces.extend(keys)
+        pieces.extend(args)
+
+        return await self.execute_command(
+            CommandName.CRON, *pieces, callback=SimpleStringCallback()
+        )
 
     @keydb_command(
         CommandName.EXPIREMEMBER,
@@ -136,4 +239,100 @@ class KeyDB(Redis[AnyStr]):
         ]
         return await self.execute_command(
             CommandName.EXPIREMEMBERAT, *pieces, callback=BoolCallback()
+        )
+
+    @keydb_command(
+        CommandName.PEXPIREMEMBERAT,
+        CommandGroup.GENERIC,
+    )
+    async def pexpirememberat(
+        self,
+        key: KeyT,
+        subkey: KeyT,
+        unix_time_milliseconds: Union[int, datetime.datetime],
+    ) -> bool:
+        """
+        Set the expiration for a subkey as a UNIX timestamp in milliseconds
+        """
+        pieces: CommandArgList = [
+            key,
+            subkey,
+            normalized_time_milliseconds(unix_time_milliseconds),
+        ]
+        return await self.execute_command(
+            CommandName.PEXPIREMEMBERAT, *pieces, callback=BoolCallback()
+        )
+
+    @keydb_command(CommandName.HRENAME, group=CommandGroup.HASH)
+    async def hrename(
+        self, key: KeyT, source_field: ValueT, destination_field: ValueT
+    ) -> bool:
+        """
+        Rename a field :paramref:`source_field` to :paramref:`destination_field`
+        in hash :param:`key`
+        """
+
+        return await self.execute_command(
+            CommandName.HRENAME,
+            key,
+            source_field,
+            destination_field,
+            callback=BoolCallback(),
+        )
+
+    @keydb_command(CommandName.MEXISTS, readonly=True, group=CommandGroup.GENERIC)
+    async def mexists(self, keys: Iterable[KeyT]) -> Tuple[bool, ...]:
+        """
+        Returns a tuple of bools in the same order as :paramref:`keys`
+        denoting whether the keys exist
+        """
+
+        return await self.execute_command(
+            CommandName.MEXISTS, *keys, callback=BoolsCallback()
+        )
+
+    @keydb_command(
+        CommandName.OBJECT_LASTMODIFIED, readonly=True, group=CommandGroup.GENERIC
+    )
+    async def object_lastmodified(self, key: KeyT) -> int:
+        """
+        Returns the time elapsed (in seconds) since the key was last modified.
+        This differs from idletime as it is not affected by reads of a key.
+
+        :return: The time in seconds since the last modification
+        """
+
+        return await self.execute_command(
+            CommandName.OBJECT_LASTMODIFIED, key, callback=IntCallback()
+        )
+
+    @keydb_command(CommandName.PTTL, readonly=True, group=CommandGroup.GENERIC)
+    async def pttl(self, key: KeyT, subkey: Optional[ValueT] = None) -> int:
+        """
+        Returns the number of milliseconds until the key :paramref:`key` will expire.
+        If :paramref:`subkey` is provided the response will be for the subkey.
+
+        :return: TTL in milliseconds, or a negative value in order to signal an error
+        """
+        pieces: CommandArgList = [key]
+        if subkey is not None:
+            pieces.append(subkey)
+
+        return await self.execute_command(
+            CommandName.PTTL, *pieces, callback=IntCallback()
+        )
+
+    @keydb_command(CommandName.TTL, readonly=True, group=CommandGroup.GENERIC)
+    async def ttl(self, key: KeyT, subkey: Optional[ValueT] = None) -> int:
+        """
+        Get the time to live for a key (or subkey) in seconds
+
+        :return: TTL in seconds, or a negative value in order to signal an error
+        """
+
+        pieces: CommandArgList = [key]
+        if subkey is not None:
+            pieces.append(subkey)
+        return await self.execute_command(
+            CommandName.TTL, *pieces, callback=IntCallback()
         )
--- tests/experimental/test_keydb.py
+++ tests/experimental/test_keydb.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 import asyncio
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytest
 
+from coredis import ResponseError
 from tests.conftest import targets
 
 
@@ -14,6 +15,66 @@ from tests.conftest import targets
 )
 @pytest.mark.asyncio()
 class TestKeyDBCommands:
+    async def test_bitop_lshift(self, client, _s):
+        await client.bitfield("a").set("u16", 0, 42).exc()
+        assert 42 == (await client.bitfield("a").get("u16", 0).exc())[0]
+        await client.bitop(["a"], "lshift", "a", 1)
+        assert 84 == (await client.bitfield("a").get("u16", 0).exc())[0]
+
+    async def test_bitop_rshift(self, client, _s):
+        await client.bitfield("a").set("u16", 0, 42).exc()
+        assert 42 == (await client.bitfield("a").get("u16", 0).exc())[0]
+        await client.bitop(["a"], "rshift", "a", 1)
+        assert 21 == (await client.bitfield("a").get("u16", 0).exc())[0]
+
+    async def test_cron_single(self, client, _s):
+        scrpt = """
+        local value = tonumber(redis.call("LPOP", KEYS[1]))
+        redis.call("LPUSH", KEYS[1], value + tonumber(ARGV[1]))
+        """
+        await client.lpush("cs", [1])
+        assert await client.cron(
+            "single", False, delay=10, script=scrpt, keys=["cs"], args=[1]
+        )
+        assert await client.lrange("cs", 0, -1) == [_s("1")]
+        await asyncio.sleep(0.2)
+        assert await client.lrange("cs", 0, -1) == [_s("2")]
+
+    async def test_cron_single_start_at(self, client, _s):
+        scrpt = """
+        local value = tonumber(redis.call("LPOP", KEYS[1]))
+        redis.call("LPUSH", KEYS[1], value + tonumber(ARGV[1]))
+        """
+        await client.lpush("css", [1])
+        assert await client.cron(
+            "single",
+            False,
+            delay=1,
+            start=datetime.now() + timedelta(milliseconds=100),
+            script=scrpt,
+            keys=["css"],
+            args=[1],
+        )
+        assert await client.lrange("css", 0, -1) == [_s("1")]
+        await asyncio.sleep(0.2)
+        assert await client.lrange("css", 0, -1) == [_s("2")]
+
+    async def test_cron_repeat(self, client, _s):
+        scrpt = """
+        local value = tonumber(redis.call("LPOP", KEYS[1]))
+        redis.call("LPUSH", KEYS[1], value + tonumber(ARGV[1]))
+        """
+        await client.lpush("cr", [1])
+        assert await client.cron(
+            "repeat", True, delay=100, script=scrpt, keys=["cr"], args=[1]
+        )
+        await asyncio.sleep(0.5)
+        value = int((await client.lrange("cr", 0, -1))[0])
+        assert value > 2, value
+        assert await client.delete(["repeat"])
+        await asyncio.sleep(0.5)
+        assert await client.lrange("cr", 0, -1) == [_s(value)]
+
     async def test_expiremember_hash(self, client, _s):
         await client.hset("a", {"b": "1"})
         assert await client.hget("a", "b") == _s("1")
@@ -34,26 +95,85 @@ class TestKeyDBCommands:
         assert await client.expiremember("a", "b", 1, b"ms")
         await asyncio.sleep(0.2)
         assert not await client.zrandmember("a", count=1, withscores=True)
-        assert not await client.smembers("a")
 
-    async def test_expirememeberat_hash(self, client, _s):
+    async def test_expirememberat_hash(self, client, _s):
         await client.hset("a", {"b": "1"})
         assert await client.hget("a", "b") == _s("1")
         assert await client.expirememberat("a", "b", datetime.now())
+        assert 0 == await client.ttl("a", "b")
         await asyncio.sleep(0.2)
         assert not await client.hget("a", "b")
 
-    async def test_expirememeberat_set(self, client, _s):
+    async def test_expirememberat_set(self, client, _s):
         await client.sadd("a", {"b"})
         assert await client.smembers("a") == {_s("b")}
         assert await client.expirememberat("a", "b", datetime.now())
+        assert 0 == await client.ttl("a", "b")
         await asyncio.sleep(0.2)
         assert not await client.smembers("a")
 
-    async def test_expirememeberat_sorted_set(self, client, _s):
+    async def test_expirememberat_sorted_set(self, client, _s):
         await client.zadd("a", {"b": 1.0})
         assert await client.zrandmember("a", count=1) == [_s("b")]
         assert await client.expirememberat("a", "b", datetime.now())
+        assert 0 == await client.ttl("a", "b")
         await asyncio.sleep(0.2)
         assert not await client.zrandmember("a", count=1, withscores=True)
+
+    async def test_pexpirememberat_hash(self, client, _s):
+        await client.hset("a", {"b": "1"})
+        assert await client.hget("a", "b") == _s("1")
+        assert await client.pexpirememberat(
+            "a", "b", datetime.now() + timedelta(milliseconds=100)
+        )
+        assert 0 < await client.pttl("a", "b")
+        await asyncio.sleep(0.2)
+        assert not await client.hget("a", "b")
+
+    async def test_pexpirememberat_set(self, client, _s):
+        await client.sadd("a", {"b"})
+        assert await client.smembers("a") == {_s("b")}
+        assert await client.pexpirememberat(
+            "a", "b", datetime.now() + timedelta(milliseconds=100)
+        )
+        assert 0 < await client.pttl("a", "b")
+        await asyncio.sleep(0.2)
         assert not await client.smembers("a")
+
+    async def test_pexpirememberat_sorted_set(self, client, _s):
+        await client.zadd("a", {"b": 1.0})
+        assert await client.zrandmember("a", count=1) == [_s("b")]
+        assert await client.pexpirememberat(
+            "a", "b", datetime.now() + timedelta(milliseconds=100)
+        )
+        assert 0 < await client.pttl("a", "b")
+        await asyncio.sleep(0.2)
+        assert not await client.zrandmember("a", count=1, withscores=True)
+
+    async def test_hrename(self, client, _s):
+        await client.hset("a", {"b": 2, "c": 3, "d": 4})
+        assert await client.hrename("a", "b", "f")
+        assert await client.hgetall("a") == {
+            _s("c"): _s(3),
+            _s("d"): _s(4),
+            _s("f"): _s(2),
+        }
+        with pytest.raises(ResponseError):
+            await client.hrename("a", "b", "f")
+
+    async def test_mexists(self, client, _s):
+        await client.mset({"a": 1, "b": 2, "c": 3, "e": 5})
+        await client.mexists(["a", "b", "c", "d", "e"]) == (
+            True,
+            True,
+            True,
+            True,
+            False,
+            True,
+        )
+
+    async def test_object_lastmodified(self, client, _s):
+        await client.set("a", 1)
+        lm = await client.object_lastmodified("a")
+        await asyncio.sleep(1.1)
+        assert 1 >= await client.object_lastmodified("a") - lm
