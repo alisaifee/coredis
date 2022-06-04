3fd5b6b808acb1d138f3c007d212f3cb3e1af1f2
Ali-Akber Saifee <ali@indydevs.org>
2022-06-04 05:40:49 -0700

    Add python 3.11 to CI
    - Add 3.11 wheel build
    - Update mypy to master for python 3.11 support

--- setup.py
+++ setup.py
@@ -140,6 +140,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     ext_modules=extensions,
 )
