85793239265597ebdb64b12c0d1ce945c1ed6c2c
Ali-Akber Saifee <ali@indydevs.org>
2022-06-10 09:59:40 -0700

    Update tagging script

--- tag.sh
+++ tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 make lint
-last_tag=$(git tag | sort -nr | head -n 1)
+last_tag=$(git tag | grep -v 'v' | sort -Vr | head -n 1)
 echo current version:$(python setup.py --version), current tag: $last_tag
 read -p "new version:" new_version
 last_portion=$(grep -P "^Changelog$" HISTORY.rst -5 | grep -P "^v\d+.\d+")
