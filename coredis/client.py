e5cca6b08636650089fb916c7c2329ede4cc834b
Ali-Akber Saifee <ali@indydevs.org>
2022-06-29 19:28:32 -0700

    Fix incorrect implementation of non_atomic_cross_slot routing
    The option only worked when all nodes had keys allocated
    in the command and would fail if that was not the case

--- coredis/client.py
+++ coredis/client.py
@@ -1506,8 +1506,9 @@ class RedisCluster(
                     await connection.send_command(
                         command, *node_arg_mapping[node["name"]]
                     )
+                elif node_arg_mapping:
+                    continue
                 else:
-                    node_arg_mapping[node["name"]] = args
                     await connection.send_command(command, *args)
                 if not self.noreply:
                     res[node["name"]] = callback(
