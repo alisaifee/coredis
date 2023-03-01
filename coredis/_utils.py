ad8a82f9e04a2f171c782ef965ecf707afafec02
Ali-Akber Saifee <ali@indydevs.org>
2023-03-01 07:59:35 -0800

    Reorder utility classes

--- coredis/_utils.py
+++ coredis/_utils.py
@@ -94,6 +94,43 @@ class EncodingInsensitiveDict(ObjectProxy):  # type: ignore
         return repr(self.__wrapped__)
 
 
+@enum.unique
+class CaseAndEncodingInsensitiveEnum(bytes, enum.Enum):
+    __decoded: Set[StringT]
+
+    @property
+    def variants(self) -> Set[StringT]:
+        if not hasattr(self, "__decoded"):
+            decoded = str(self)
+            self.__decoded = {
+                self.value.lower(),  # type: ignore
+                self.value,  # type: ignore
+                decoded.lower(),
+                decoded.upper(),
+            }
+        return self.__decoded
+
+    def __eq__(self, other: object) -> bool:
+        """
+        Since redis tokens are case insensitive allow mixed case
+        Additionally allow strings to be passed in instead of
+        bytes.
+        """
+
+        if other:
+            if isinstance(other, self.__class__):
+                return bool(self.value == other.value)
+            else:
+                return other in self.variants
+        return False
+
+    def __str__(self) -> str:
+        return self.decode("latin-1")
+
+    def __hash__(self) -> int:
+        return hash(self.value)
+
+
 def b(x: ResponseType, encoding: Optional[str] = None) -> bytes:
     if isinstance(x, bytes):
         return x
@@ -458,43 +495,6 @@ except ImportError:
                 key = key[start + 1 : end]
 
         return crc16(key) % 16384
-
-
-@enum.unique
-class CaseAndEncodingInsensitiveEnum(bytes, enum.Enum):
-    __decoded: Set[StringT]
-
-    @property
-    def variants(self) -> Set[StringT]:
-        if not hasattr(self, "__decoded"):
-            decoded = str(self)
-            self.__decoded = {
-                self.value.lower(),  # type: ignore
-                self.value,  # type: ignore
-                decoded.lower(),
-                decoded.upper(),
-            }
-        return self.__decoded
-
-    def __eq__(self, other: object) -> bool:
-        """
-        Since redis tokens are case insensitive allow mixed case
-        Additionally allow strings to be passed in instead of
-        bytes.
-        """
-
-        if other:
-            if isinstance(other, self.__class__):
-                return bool(self.value == other.value)
-            else:
-                return other in self.variants
-        return False
-
-    def __str__(self) -> str:
-        return self.decode("latin-1")
-
-    def __hash__(self) -> int:
-        return hash(self.value)
 
 
 __all__ = [
