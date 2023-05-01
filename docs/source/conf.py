0a9cda7bf5a314c4c5042a83606f006c112a807d
Ali-Akber Saifee <ali@indydevs.org>
2023-05-01 13:29:46 -0700

    Add sphinx sitemap extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,9 @@ html_css_files = [
     "https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;700&family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
 ]
 
+html_baseurl = 'https://coredis.readthedocs.io/'
+sitemap_url_scheme = "en/stable/{link}"
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosectionlabel",
@@ -45,6 +48,7 @@ extensions = [
     "sphinx_copybutton",
     "sphinx_issues",
     "sphinx_paramlinks",
+    "sphinx_sitemap",
     "sphinxcontrib.programoutput",
     "sphinxext.opengraph",
 ]
