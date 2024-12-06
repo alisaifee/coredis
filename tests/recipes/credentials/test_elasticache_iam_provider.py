7f26fa69b547a68a3e091921673b60faf789a168
Ali-Akber Saifee <ali@indydevs.org>
2024-12-05 17:28:33 -0800

    Add basic test for ElastiCache IAM provider

---
+++ tests/recipes/credentials/test_elasticache_iam_provider.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from moto import mock_aws
+
+from coredis.recipes.credentials import ElastiCacheIAMProvider
+
+
+class TestElastiCacheIAMProvider:
+    async def test_get_credentials(self):
+        with mock_aws():
+            provider = ElastiCacheIAMProvider("test_user", "test_cluster")
+            user_pass = await provider.get_credentials()
+            assert user_pass.username == "test_user"
+            assert "test_cluster/?Action=connect&User=test_user" in user_pass.password
