9bca13ef41537adb54b6f73d30da55ea60189739
Ali-Akber Saifee <ali@indydevs.org>
2025-05-14 11:55:38 -0700

    Change all redis command methods to return a task
    Instead of implementing each redis command as a coroutine
    they are now regular methods that return a subclass of asyncio.Task.
    
    This is (mostly) backward compatible with an advantage of allowing construction
    of the task in a non async method and then being awaited in an async context.

--- coredis/commands/task.py
+++ coredis/commands/task.py
@@ -5,9 +5,10 @@ from typing import Any
 
 from coredis._protocols import AbstractExecutor
 from coredis.response._callbacks import ResponseCallback
-from coredis.typing import ExecutionParameters, TypeVar, ValueT
+from coredis.typing import Callable, ExecutionParameters, TypeVar, ValueT
 
 TaskResponse = TypeVar("TaskResponse", covariant=True)
+TransformedResponse = TypeVar("TransformedResponse")
 
 
 class CommandTask(asyncio.Task[TaskResponse]):
@@ -29,3 +30,8 @@ class CommandTask(asyncio.Task[TaskResponse]):
 
     async def run(self) -> TaskResponse:
         return await self.client.execute_command(self, self.callback, **self.execution_parameters)
+
+    async def transform(
+        self, transformer: Callable[[TaskResponse], TransformedResponse]
+    ) -> TransformedResponse:
+        return transformer(await self)
