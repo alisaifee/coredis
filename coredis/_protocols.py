f250a3eaf1e2192d84280f4d9f2e6324b63141e2
Ali-Akber Saifee <ali@indydevs.org>
2025-05-28 11:40:40 -0700

    Remove unused pipeline related protocols

--- coredis/_protocols.py
+++ coredis/_protocols.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
 import asyncio
-from types import TracebackType
 
 from typing_extensions import runtime_checkable
 
 from coredis.response._callbacks import NoopCallback
 from coredis.typing import (
     TYPE_CHECKING,
-    Awaitable,
     Callable,
     ExecutionParameters,
     KeyT,
@@ -48,15 +46,6 @@ class AbstractExecutor(Protocol):
 
 
 @runtime_checkable
-class SupportsPipeline(Protocol):  # noqa
-    async def pipeline(
-        self,
-        transaction: bool | None = True,
-        watches: Parameters[StringT] | None = None,
-    ) -> SupportsWatch: ...
-
-
-@runtime_checkable
 class SupportsScript(Protocol[T_co]):  # noqa
     async def evalsha(
         self,
@@ -73,22 +62,6 @@ class SupportsScript(Protocol[T_co]):  # noqa
     ) -> ResponseType: ...
 
     async def script_load(self, script: StringT) -> T_co: ...
-
-
-@runtime_checkable
-class SupportsWatch(Protocol):  # noqa
-    async def __aenter__(self) -> SupportsWatch: ...
-
-    async def __aexit__(
-        self,
-        exc_type: type[BaseException] | None,
-        exc_value: BaseException | None,
-        traceback: TracebackType | None,
-    ) -> Awaitable[bool | None]: ...
-
-    async def watch(self, *keys: KeyT) -> bool: ...
-
-    async def execute(self, raise_on_error: bool = True) -> tuple[object, ...]: ...
 
 
 @runtime_checkable
