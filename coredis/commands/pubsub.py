93389805d3d1ab81a55ec9bbb81710945e09c138
Ali-Akber Saifee <ali@indydevs.org>
2025-06-19 10:30:19 -0700

    Fix busy loop in pubsub consumer task
    When the pubsub instance has not subscribed to a channel
    or pattern and has been initialized the consumer task
    ends up in a busy loop. Instead if the consumer task is
    started and not yet subscribed await an event set whenever
    subscribe or psubscribe is called

--- coredis/commands/pubsub.py
+++ coredis/commands/pubsub.py
@@ -115,6 +115,7 @@ class BasePubSub(Generic[AnyStr, PoolT]):
         }
         self._message_queue: asyncio.Queue[PubSubMessage | None] = asyncio.Queue()
         self._consumer_task: asyncio.Task[None] | None = None
+        self._subscribed = asyncio.Event()
         self.reset()
 
     @property
@@ -173,6 +174,7 @@ class BasePubSub(Generic[AnyStr, PoolT]):
         # subscribe twice to these patterns, once for the command and again
         # for the reconnection.
         self.patterns.update(new_patterns)
+        self._subscribed.set()
 
     async def punsubscribe(self, *patterns: StringT) -> None:
         """
@@ -204,6 +206,7 @@ class BasePubSub(Generic[AnyStr, PoolT]):
         # subscribe twice to these channels, once for the command and again
         # for the reconnection.
         self.channels.update(new_channels)
+        self._subscribed.set()
 
     async def unsubscribe(self, *channels: StringT) -> None:
         """
@@ -432,6 +435,9 @@ class BasePubSub(Generic[AnyStr, PoolT]):
                 if inspect.isawaitable(handler_response):
                     await handler_response
                 return None
+        if not (self.channels or self.patterns):
+            self._subscribed.clear()
+
         return message
 
     @deprecated(
@@ -482,7 +488,7 @@ class BasePubSub(Generic[AnyStr, PoolT]):
                     ):
                         self._message_queue.put_nowait(await self.handle_message(response))
                 else:
-                    await asyncio.sleep(0)
+                    await self._subscribed.wait()
             except asyncio.CancelledError:
                 break
             except ConnectionError:
@@ -580,6 +586,7 @@ class BasePubSub(Generic[AnyStr, PoolT]):
         self.channels = {}
         self.patterns = {}
         self.initialized = False
+        self._subscribed.clear()
 
     async def reset_connections(self, exc: BaseException | None = None) -> None:
         pass
@@ -769,6 +776,7 @@ class ShardedPubSub(BasePubSub[AnyStr, "coredis.pool.ClusterConnectionPool"]):
         for new_channel in new_channels.keys():
             await self.execute_command(CommandName.SSUBSCRIBE, new_channel, sharded=True)
         self.channels.update(new_channels)
+        self._subscribed.set()
 
     async def unsubscribe(self, *channels: StringT) -> None:
         """
@@ -981,6 +989,7 @@ class ShardedPubSub(BasePubSub[AnyStr, "coredis.pool.ClusterConnectionPool"]):
         self.channels = {}
         self.patterns = {}
         self.initialized = False
+        self._subscribed.clear()
 
     async def aclose(self) -> None:
         """
