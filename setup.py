f0ae313e3312ae7041d7285be9ed9353486474f8
Ali-Akber Saifee <ali@indydevs.org>
2025-07-12 11:39:54 -0700

    Remove outdated license classifier

--- setup.py
+++ setup.py
@@ -147,7 +147,6 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.10",
