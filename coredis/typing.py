7f345b53a63d4c001bb85e2fd00b78637bfcc47e
mhasan502 <mhasan502@gmail.com>
2025-10-10 10:19:25 +0600

    Refactor imports for `infer_hint` with TYPE_CHECKING support for mypy

--- coredis/typing.py
+++ coredis/typing.py
@@ -47,10 +47,15 @@ from beartype import __version__ as beartype_version
 from beartype import beartype
 from packaging import version
 
-if version.parse(beartype_version) < version.parse("0.22"):
-    from beartype.door import infer_hint
+if TYPE_CHECKING:
+    infer_hint: Callable[..., Any]
+    is_bearable: Callable[[Any, Any], bool]
+    is_subhint: Callable[[Any, Any], bool]
 else:
-    from beartype.bite import infer_hint
+    if version.parse(beartype_version) < version.parse("0.22"):
+        from beartype.door import infer_hint
+    else:
+        from beartype.bite import infer_hint
 
 from beartype.door import is_bearable, is_subhint
 from typing_extensions import (
