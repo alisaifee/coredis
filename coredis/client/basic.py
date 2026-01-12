f3c597b65f49500196470c5b484abc9b8498a34d
Ali-Akber Saifee <ali@indydevs.org>
2026-01-12 14:27:52 -0800

    Mark RESP2 as deprecated

--- coredis/client/basic.py
+++ coredis/client/basic.py
@@ -194,6 +194,8 @@ class Client(
             2,
             3,
         }, "Protocol version can only be one of {2,3}"
+        if connection_protocol_version == 2:
+            warnings.warn("Support for RESP2 will be removed in version 6.x", DeprecationWarning)
         self.protocol_version = connection_protocol_version
         self.server_version: Version | None = None
         self.verify_version = verify_version
